9002a26aeac9beb323991984817913e49a783155
Cari Licor <Carilicor@gmail.com>
2022-10-31 16:22:52 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
 import SingleCoffee from './SingleCoffee';
 
 function App() {
+  const [data, setData] = useState([{name: 'Colada', price: '40cents'}])
+
   const getCoffee = () => {
     fetch('https://api.sampleapis.com/coffee/hot')
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => res.json()) // convert to json
+      .then(data => setData(data)) //get formatted data
       .catch(err => console.error(err))
+    
   }
+
+  console.log('my data', data)
   return (
     <div className="App">
       <main>
         <h1>Coffes by Sample API</h1>
-        <button onClick={getCoffee}>Get me Coffee</button>
+        <button onClick={() => getCoffee()}>Get me Coffee</button>
         <div className='menu-items'>
-        <SingleCoffee name= 'Black American Coffee' price= '$$$$' description='yummy'/>
-        <SingleCoffee  name='Cafe Cubano' price='Mucho Mula' description='so good' />
-        <SingleCoffee />
-        <SingleCoffee />
-        <SingleCoffee />
-        <SingleCoffee />
-        
-        {/* <div className='menu-items'>
+          {data.map(item => {
+              console.log('each item', item)
+            return <SingleCoffee  entireObject={item}/> 
+          })}
+
+          {/* <SingleCoffee name='Black American Coffee' price='$$$$' description='yummy' />
+          <SingleCoffee name='Cafe Cubano' price='Mucho Mula' description='so good' />
+          <SingleCoffee />
+          <SingleCoffee />
+          <SingleCoffee />
+          <SingleCoffee /> */}
+
+          {/* <div className='menu-items'>
           <div className="menu-items__item">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/640px-A_small_cup_of_coffee.JPG" 
               alt="" />
@@ -31,7 +42,7 @@ function App() {
               <p>Black coffee is as simple as it gets</p>
             </div>
           </div> */}
-         </div>
+        </div>
       </main>
     </div>
   );
--- src/SingleCoffee.js
+++ src/SingleCoffee.js
@@ -1,14 +1,27 @@
-export default function SingleCoffee(props) {
-    console.log('pros-here', props.name)
+export default function SingleCoffee({name, price, description}) {
+    
+    // const exampleProps = {
+    //     description: 'yummy',
+    //     name: 'Cafe Cubano',
+    //     price: 'Mucho Mula',
+    // }
+    // const description = exampleProps.description  --longer version
+    // const name = exampleProps.name
+    // const price = exampleProps.price
+    
+    //const {description, name, price} = exampleProps //destructuring 
+
+    
+    //console.log('pros-here', name)
     return(
         
           <div className="menu-items__item">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/640px-A_small_cup_of_coffee.JPG" 
               alt="" />
             <div>
-              <h3>{props.name}</h3>
-              <span>Price: {props.price}</span>
-                <p>{props.description}</p>
+              <h3>{name}</h3>
+              <span>{price}</span>
+                <p>{description}</p>
               <p>Black coffee is as simple as it gets</p>
             </div>
           </div>
